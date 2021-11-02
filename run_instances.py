5cd2c55e56a80d56dea348e0644d548be83fe62e
Mark Scheibner <mark.scheibner@student.uni-luebeck.de>
2021-11-02 13:22:38 +0100

    modify script to use multiprocessing and allowlist

--- run_instances.py
+++ run_instances.py
@@ -1,29 +1,44 @@
 import subprocess
 from os import listdir
+import os
 import sys
+from multiprocessing import Pool
 
-if len(sys.argv) != 3:
-	print(f"Usage: python {sys.argv[0]} /path/to/instances timeout")
+if len(sys.argv) != 5:
+	print(f"Usage: python {sys.argv[0]} /path/to/instances /path/to/instancelist timeout threads")
 	exit(1)
 
-instances = listdir(sys.argv[1])
+
+instances = []
+with open(sys.argv[2]) as instancelist:
+	instances = list(map(lambda s: s.strip(), instancelist.readlines()))
 num_instances = len(instances)
-timeout = int(sys.argv[2])
+timeout = int(sys.argv[3])
+threads = int(sys.argv[4])
 
-print("name, mode, nodes, edges, maxdeg, mindeg, preproc-reduction, success")
+print("name, mode, nodes, edges, components, maxdeg, mindeg, preproc-reduction, tree-width, success")
 
-for instance in instances:
+def do_instance(instance):
+	results = []
+
 	for mode in ["-p", "-d", "-i"]:
 		try:
 			command = ["cargo", "run", "--release", "--quiet", "--", "--file", f"{sys.argv[1]}/{instance}", "-c", mode]
 			p = subprocess.run(command, stdout=subprocess.PIPE, encoding="UTF-8", timeout=timeout)
 			if p.returncode == 0:
-				print(f"{instance}, {mode}, {p.stdout.strip()}, 0")
+				results.append(f"{instance}, {mode}, {p.stdout.strip()}, 0")
 			else:
-				print(f"{instance}, {mode}, {p.stdout.strip()}, 1")
+				results.append(f"{instance}, {mode}, {p.stdout.strip()}, 1")
 		except subprocess.TimeoutExpired as exc:
 			if exc.stdout:
-				print(f"{instance}, {mode}, {exc.stdout.strip()}, 2")
+				results.append(f"{instance}, {mode}, {exc.stdout.strip()}, 2")
 			else:
-				print(f"{instance}, {mode}, -1, -1, -1, -1, -1")
-	sys.stdout.flush()
+				results.append(f"{instance}, {mode}, -1, -1, -1, -1, -1, -1, -1, -1")
+	
+	return results
+
+results = []
+with Pool(threads) as p:
+	for result in p.map(do_instance, instances):
+		for run in result:
+			print(run)
