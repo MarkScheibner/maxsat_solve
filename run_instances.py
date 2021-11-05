cd42f5bf3bd2e03867ca1dbb9b1416154a0c2cef
Mark Scheibner <mark.scheibner@student.uni-luebeck.de>
2021-11-05 19:24:24 +0100

    use instancelist format with instance and mode

--- run_instances.py
+++ run_instances.py
@@ -11,34 +11,27 @@ if len(sys.argv) != 5:
 
 instances = []
 with open(sys.argv[2]) as instancelist:
-	instances = list(map(lambda s: s.strip(), instancelist.readlines()))
-num_instances = len(instances)
+	instances = list(map(lambda s: s.strip().split(", "), instancelist.readlines()))
 timeout = int(sys.argv[3])
 threads = int(sys.argv[4])
 
 print("name, mode, nodes, edges, components, maxdeg, mindeg, preproc-reduction, tree-width, success")
 
 def do_instance(instance):
-	results = []
+	name, mode = instance
+	try:
+		command = ["cargo", "run", "--release", "--quiet", "--", "--file", f"{sys.argv[1]}/{name}", "-c", mode]
+		p = subprocess.run(command, stdout=subprocess.PIPE, encoding="UTF-8", timeout=timeout)
+		if p.returncode == 0:
+			return f"{name}, {mode}, {p.stdout.strip()}, 0"
+		else:
+			return f"{name}, {mode}, {p.stdout.strip()}, 1"
+	except subprocess.TimeoutExpired as exc:
+		if exc.stdout:
+			return f"{name}, {mode}, {exc.stdout.strip()}, 2"
+		else:
+			return f"{name}, {mode}, -1, -1, -1, -1, -1, -1, -1, -1"
 
-	for mode in ["-p", "-d", "-i"]:
-		try:
-			command = ["cargo", "run", "--release", "--quiet", "--", "--file", f"{sys.argv[1]}/{instance}", "-c", mode]
-			p = subprocess.run(command, stdout=subprocess.PIPE, encoding="UTF-8", timeout=timeout)
-			if p.returncode == 0:
-				results.append(f"{instance}, {mode}, {p.stdout.strip()}, 0")
-			else:
-				results.append(f"{instance}, {mode}, {p.stdout.strip()}, 1")
-		except subprocess.TimeoutExpired as exc:
-			if exc.stdout:
-				results.append(f"{instance}, {mode}, {exc.stdout.strip()}, 2")
-			else:
-				results.append(f"{instance}, {mode}, -1, -1, -1, -1, -1, -1, -1, -1")
-	
-	return results
-
-results = []
 with Pool(threads) as p:
 	for result in p.map(do_instance, instances):
-		for run in result:
-			print(run)
+		print(result)
